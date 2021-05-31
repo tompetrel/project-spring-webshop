ac47457795b5441712071ed7c182ce6cf3490348
tompetrel <huutaimarvel@gmail.com>
2021-05-31 17:36:49 +0700

    Start

---
+++ tech-webshop/src/main/java/edu/poly/webshop/TechWebshopApplication.java
@@ -0,0 +1,13 @@
+package edu.poly.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechWebshopApplication.class, args);
+	}
+
+}
